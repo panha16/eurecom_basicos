ebf41b5263bb3233a725280e477f0aa74e27a765
ung <ung@eurecom.frm>
2022-10-24 15:58:27 +0200

    add inode and superblock struct

--- src/fs.h
+++ src/fs.h
@@ -1,16 +1,22 @@
 #define DATABLOCK_SIZE 4096
 
+
 typedef struct{
-    int db_count = 15;
-    int inode_count = 10;
-    char inode_table_pt;
-    char free_inode_pt;
-    char free_db_pt;
+    int db_count = 2000;
+    int inode_count = 100;
+    char* inode_table_pt;
+    char* free_inode_pt;
+    char* free_db_pt;
 }superblock;
 
 typedef struct{
     int inode_number;
     char inode_type;
     char inode_rights;
-    
+    char timestamp_access;
+    char timestamp_modify;
+    char timestamp_metadata;
+    int db_size;
+    int db_count;
+    int * db_pt;    //datablock pointer
 }inode;
