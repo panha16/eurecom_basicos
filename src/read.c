20bf43f8d276f7eaa665b5c343b6f6d558623d79
ung <ung@eurecom.fr>
2022-12-02 13:28:03 +0100

    aaaaa

--- src/read.c
+++ src/read.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 #include "fs.h"
 
 int read_file(char* fs_name,char *path_to_file, char* dbs, inode_t* table){
 
     load_inodes(fs_name, table);
+    
     //get inode from filename
     inode_t read_inode = get_inode(path_to_file,table);
     if (read_inode.inode_number == -1 ){
@@ -14,9 +16,13 @@ int read_file(char* fs_name,char *path_to_file, char* dbs, inode_t* table){
     }
     
     int db_number = read_inode.db_pt;
+    char* content = malloc(DATABLOCK_SIZE*read_inode.db_count);
+    int k = 0;
     for (int i = db_number ; i < db_number + (DATABLOCK_SIZE*read_inode.db_count) ; i++){
         if (dbs[i]=='\0') break;
-        printf("%c\n",dbs[i]);
+        strncat(content,&dbs[k],1);
+        k++;
     }
+    printf("%s\n",content);
     return 0;
 }
